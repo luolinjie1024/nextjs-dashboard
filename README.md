ad7956a01360d8fc004b484807ea205f8cf8f725
linjie.luo <linjie.luo@tcl.com>
2024-12-12 08:00:55 +0800

    feat:增加登录功能

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 This is the starter template for the Next.js App Router Course. It contains the starting code for the dashboard application.
 
 For more information, see the [course curriculum](https://nextjs.org/learn) on the Next.js Website.
+
+
+完整功能的代码：
+
+https://github.com/qufei1993/nextjs-learn-example/blob/main/middleware.ts
+
+
+
--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,3 +1,6 @@
+// 用于指示某个函数或代码块应该只在客户端执行，而不是在服务器端渲染时执行
+'use client';
+
 import { lusitana } from '@/app/ui/fonts';
 import {
   AtSymbolIcon,
@@ -6,10 +9,15 @@ import {
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
+import { useFormState, useFormStatus } from 'react-dom';
+import { authenticate } from '@/app/lib/actions';
 
 export default function LoginForm() {
+
+  const [errorMessage, dispatch] = useFormState(authenticate, undefined);
+
   return (
-    <form className="space-y-3">
+    <form  action={dispatch}  className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
           Please log in to continue.
@@ -55,13 +63,33 @@ export default function LoginForm() {
             </div>
           </div>
         </div>
-        <Button className="mt-4 w-full">
-          Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
-        </Button>
-        <div className="flex h-8 items-end space-x-1">
-          {/* Add form errors here */}
+        <LoginButton />
+        <div
+            className="flex h-8 items-end space-x-1"
+            aria-live="polite"
+            aria-atomic="true"
+        >
+          {errorMessage && (
+              <>
+                <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
+                <p className="text-sm text-red-500">{errorMessage}</p>
+              </>
+          )}
         </div>
       </div>
     </form>
   );
 }
+function LoginButton() {
+  // useFormStatus钩子返回一个对象，其中包含pending属性，该属性是一个布尔值，表示表单是否正在提交中。
+  // 如果表单正在提交，pending为true；如果表单提交完成或未开始，pending为false。
+  const { pending } = useFormStatus();
+
+  return (
+      <Button className="mt-4 w-full" aria-disabled={pending}>
+        Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+      </Button>
+  );
+}
+//登录用户名密码：
+// user@nextmail.com 123456
